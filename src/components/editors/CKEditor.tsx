a5f23f61c44128fdc9ebb0344cec0add5e519cfb
Steven Salka <steven.salka@amplitude.com>
2022-10-03 17:07:05 -0700

    main full-width ckeditor

--- src/components/editors/CKEditor.tsx
+++ src/components/editors/CKEditor.tsx
@@ -5,27 +5,29 @@ import InlineEditor from '@ckeditor/ckeditor5-build-inline';
 import { useSetRecoilState } from 'recoil';
 import { defaultText, jsonState } from '@/content';
 
+import './CKEditor.css';
+
 export default () => {
   const setJson = useSetRecoilState(jsonState);
   return (
     <CKEditor
       editor={InlineEditor}
       data="<p>Hello from CKEditor 5!</p>"
-      onReady={(editor) => {
+      onReady={(editor: any) => {
         console.log('Editor is ready to use', editor);
         editor.setData(defaultText);
         setJson(editor.getData());
       }}
-      onChange={(event, editor) => {
+      onChange={(event: any, editor: any) => {
         // NOTE: data is a string of HTML... not sure this will suffice
         // for custom elements like charts, cohorts etc
         const data = editor.getData();
         setJson(data);
       }}
-      onBlur={(event, editor) => {
+      onBlur={(event: any, editor: any) => {
         console.log('Blur.', editor);
       }}
-      onFocus={(event, editor) => {
+      onFocus={(event: any, editor: any) => {
         console.log('Focus.', editor);
       }}
     />
