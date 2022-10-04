02235a78d2bb652b878e77d10476d8aa13e86938
Steven Salka <steven.salka@amplitude.com>
2022-10-04 14:23:21 -0700

    main Tiptap loading in markdown

--- src/components/editors/Tiptap.tsx
+++ src/components/editors/Tiptap.tsx
@@ -1,23 +1,61 @@
 import clsx from 'clsx';
+import camelCase from 'lodash/camelCase';
 import { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { BubbleMenu, EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import Link from '@tiptap/extension-link';
+import { defaultMarkdownParser } from 'prosemirror-markdown';
+
 import { defaultText, jsonState } from '@/content';
 
 import './Tiptap.css';
+
+const markMap = {
+  em: 'italic',
+};
+
+// TODO: fix empty lines being removed
+function mdToJson(doc: any) {
+  return {
+    type: camelCase(doc.type.name),
+    ...(doc.type.name === 'text'
+      ? {
+          text: doc.text,
+          marks: doc.marks.map((m: any) => ({
+            type: markMap[m.type.name] || m.type.name,
+          })),
+          ...(doc.attrs && { attrs: doc.attrs }),
+        }
+      : {
+          content: doc.content.content.map(mdToJson),
+        }),
+  };
+}
+console.log(defaultMarkdownParser.parse(defaultText));
+console.log(mdToJson(defaultMarkdownParser.parse(defaultText)));
 
 // BUG BubbleMenu breaks on unmount https://github.com/ueberdosis/tiptap/issues/3135
 export default () => {
   const setJson = useSetRecoilState(jsonState);
   const editor = useEditor({
-    content: defaultText,
+    content: mdToJson(defaultMarkdownParser.parse(defaultText)),
+    // content: rawDoc,
     extensions: [
       StarterKit.configure({
         paragraph: {
           HTMLAttributes: {
             style: 'margin: 0;',
           },
+        },
+      }),
+      Link.configure({ autolink: true }).extend({
+        addKeyboardShortcuts() {
+          return {
+            // TODO open popover here where user can set link URL + text
+            'Mod-k': () =>
+              this.editor.commands.setLink({ href: 'https://amplitude.com' }),
+          };
         },
       }),
     ],
--- src/content.ts
+++ src/content.ts
@@ -1,6 +1,17 @@
 import { atom } from 'recoil';
 
-export const defaultText = 'Hello world!';
+export const defaultText = `
+### Hello world!
+
+This is a paragraph
+
+* bulleted list
+* item #2 [link](https://amplitude.com/)
+
+_sdf_
+
+\`sdf\`
+`;
 
 type Json = unknown;
 
